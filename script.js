2835477466e2602e625b37f21340b9b142da43b4
Matej Puhony <matej.puhony@seznam.cz>
2025-06-05 15:10:55 +0200

    draw line

--- script.js
+++ script.js
@@ -6,13 +6,17 @@ const scale = 10
 //                x  y
 // const point = [5, 5]
 
+function interpolate(aMin, aMax, bMin, bMax, a) {
+    if(aMax - aMin === 0) return bMin
+
+    const t = (a - aMin) / (aMax - aMin)
+    return Math.round(((1 - t) * bMin) + (t * bMax))
+}
+
 function drawPixel(point, color = '#000000') {
     ctx.fillStyle = color;
     ctx.fillRect(point[0] * scale, point[1] * scale, scale, scale);
 }
-
-
-drawPixel([0, 0], "#ff00ff")
 
 function drawLine(point1, point2, color) {
     if(point1[0] > point2[0]) {
@@ -21,19 +25,22 @@ function drawLine(point1, point2, color) {
         point1 = temp
     }
 
-    for (let x = point1[0]; x < point2[0]; x += 1) {
-        const y = point1[1]
+    const diffX = Math.abs(point1[0] - point2[0])
+    const diffY = Math.abs(point1[1] - point2[1])
 
-        drawPixel([x, y], color)
+    if(diffX > diffY) {
+        for (let x = point1[0]; x < point2[0]; x += 1) {
+            const y = interpolate(point1[0], point2[0], point1[1], point2[1], x)
+            drawPixel([x, y], color)
+        }
+    } else {
+        for (let y = point1[1]; y < point2[1]; y += 1) {
+            const x = interpolate(point1[1], point2[1], point1[0], point2[0], y)
+            drawPixel([x, y], color)
+        }
     }
 }
 
-drawLine([10, 0], [0, 0], "#ff0000")
-
-
-function interpolate(aMin, aMax, bMin, bMax, a) {
-    if(aMax - aMin === 0) return bMin
-
-    const t = (a - aMin) / (aMax - aMin)
-    return Math.round(((1 - t) * bMin) + (t * bMax))
-}
+drawLine([15, 5], [0, 0], "#ff0000")
+drawLine([10, 15], [0, 0], "#00ff00")
+drawLine([3, 0], [13, 15],"#0000ff")
