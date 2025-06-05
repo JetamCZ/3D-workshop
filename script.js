95acc77696c255c8d92c21386f15d112e80ec7d4
Matej Puhony <matej.puhony@seznam.cz>
2025-06-05 14:50:21 +0200

    draw pixel

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const scale = 5
+const scale = 10
+
+function drawPixel(point, color = '#000000') {
+    ctx.fillStyle = color;
+    ctx.fillRect(point.x * scale, point.y * scale, scale, scale);
+}
+
+
+drawPixel({x: 5, y: 5}, "#ff0000")
